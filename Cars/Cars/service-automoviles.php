a83747b3297a81bcd344d19f933c38ba3ee7b9ee
pesca79 <marmariscal@hotmail.com>
2022-01-25 23:43:09 +0100

    Update service-automoviles.php

--- Cars/Cars/service-automoviles.php
+++ Cars/Cars/service-automoviles.php
@@ -1,5 +1,6 @@
 <?php
 include 'GestionAutomoviles.php';
+//Server en Raspberry PI4 en otro server apache
 $soap = new SoapServer(null, array('uri' => 'https://marmpi4.ddns.net/cars_server/GestionAutomoviles.php'));
 $soap->setClass('GestionAutomovilesAuth');
 $soap->handle();
