505a7eced7bbf7b020287ae3f933dfa65144168d
pesca79 <marmariscal@hotmail.com>
2022-01-25 22:41:09 +0100

    Update GestionAutomoviles.php

--- Cars/Cars/GestionAutomoviles.php
+++ Cars/Cars/GestionAutomoviles.php
@@ -10,7 +10,7 @@ class GestionAutomovilesAuth {
             $user = "ies";  // usuario con el que se va conectar con MySQL
             $pass = "daw";  // contraseña del usuario
             $dbname = "coches";  // nombre de la base de datos
-            $host = "207.188.145.21";  // nombre o IP del host
+            $host = "localhost";  // nombre o IP del host
 
             $db = new PDO("mysql:host=$host; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA Base de Datos
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  //Manejo de errores con PDOException
