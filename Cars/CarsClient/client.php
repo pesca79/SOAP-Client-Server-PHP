b6780ce7d882fa338aad191b4463900cf3f93e59
pesca79 <marmariscal@hotmail.com>
2022-01-26 04:43:20 +0100

    Update client.php

--- Cars/CarsClient/client.php
+++ Cars/CarsClient/client.php
@@ -1,15 +1,18 @@
 <?php
 try {
 $client = new SoapClient(null, array(
-'uri'=>'http://localhost:8080/Cars/Cars/',
-'location'=>'http://localhost:8080/Cars/Cars/service-automoviles.php',
+'uri' => 'https://soap-automoviles.herokuapp.com/',
+'location' => 'https://soap-automoviles.herokuapp.com/service-automoviles-auth.php',
+//(No me funciona en mi hosting no entiendo pq)
+//'uri'=>'https://marmpi4.ddns.net/carserver/', 
+//'location'=>'https://marmpi4.ddns.net/carserver/service-automoviles.php',
 'trace'=>1
 ));
   $auth_params = new stdClass();
   $auth_params->username = "ies";
   $auth_params->password = "daw";
   $header_params = new SoapVar($auth_params,SOAP_ENC_OBJECT);
-  $header = new SoapHeader('https://marmpi4.ddns.net/cars_service/service-automoviles.php', 'authenticate', $header_params, false);
+  $header = new SoapHeader('https://marmpi4.ddns.net/carserver/service-automoviles.php', 'authenticate', $header_params, false);
   $client->__setSoapHeaders(array($header));
 } catch (Exception $e) {
     echo($client->__getLastResponse());
