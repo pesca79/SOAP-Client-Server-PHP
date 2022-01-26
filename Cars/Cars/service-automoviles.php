d66f26e94133a8ce93e0fb68a9da9fb091ed6adb
pesca79 <marmariscal@hotmail.com>
2022-01-26 14:39:39 +0100

    Delete Cars/Cars directory

--- Cars/Cars/service-automoviles.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'GestionAutomoviles.php';
-//Server en Raspberry PI4 en otro server apache
-$soap = new SoapServer(null, array('uri' => 'https://marmpi4.ddns.net/cars_server/GestionAutomoviles.php'));
-$soap->setClass('GestionAutomovilesAuth');
-$soap->handle();
-?>
