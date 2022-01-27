c14d814e49d7b0f78d566477ff74028ad2b93ac9
pesca79 <marmariscal@hotmail.com>
2022-01-27 01:27:18 +0100

    Update modelos.php

--- Cars/CarsClient/modelos.php
+++ Cars/CarsClient/modelos.php
@@ -13,12 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+		.imagen:hover {-webkit-transform:scale(1.5);transform:scale(1.5);}
+		.imagen {overflow:hidden;}
         figure {
-            border: 1px #cccccc solid;
-            padding: 4px;
-            margin: auto;
+        border: 1px #cccccc solid;
+        padding: 4px;
+        margin: auto;
         }
-
         figcaption {
             background-color: navy;
             color: white;
@@ -35,14 +36,14 @@
 </head>
 <body>
 <img src="Marcas/<?= strtolower($marcas[$marca]) ?>.png" alt="<?= $marcas[$marca] ?>" />
-    
+
 <h1>Modelos disponibles para la marca "<?=$marcas[$marca]?>"</h1>
 
 <?php
     foreach ($modelos as $modelo => $modelValue) {
 ?>
 
-    <figure>
+    <figure class="imagen">
         <img src="Modelos/<?=$modelos[$modelo]?>.jpg" alt="logo <?= $modelos[$modelo]?>.jpg" />
         <figcaption><?=$modelValue?></figcaption>
      </figure>
